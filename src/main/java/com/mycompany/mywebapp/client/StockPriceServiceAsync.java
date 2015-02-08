4ae0a1cbf728d6126dd7bb3af3742e08bb20975f
Egalvi <egalvi@mail.ru>
2015-02-09 01:39:38 +0300

    Initial commit of spring-gwt webapp

--- src/main/java/com/mycompany/mywebapp/client/StockPriceServiceAsync.java
+++
@@ -1,9 +0,0 @@
-package com.mycompany.mywebapp.client;
-
-import com.google.gwt.user.client.rpc.AsyncCallback;
-
-public interface StockPriceServiceAsync {
-
-    void getPrices(String[] symbols, AsyncCallback<StockPrice[]> callback);
-
-}
